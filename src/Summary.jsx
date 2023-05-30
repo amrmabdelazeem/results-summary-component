d034abad6fa635bae79121014125d42e17efad9f
Amr Elsehety <amr.abdelazeem22@gmail.com>
2023-05-30 13:10:08 +0300

    Add Summary section

--- src/Summary.jsx
+++ src/Summary.jsx
@@ -7,10 +7,10 @@ import Button from "./Button";
 function Summary(){
     return <div className="summary">
     <h3>Summary</h3>
-    <Skill category={data[0].category} icon={data[0].icon} score={data[0].score} />
-    <Skill category={data[1].category} icon={data[1].icon} score={data[1].score} />
-    <Skill category={data[2].category} icon={data[2].icon} score={data[2].score} />
-    <Skill category={data[3].category} icon={data[3].icon} score={data[3].score} />
+    <Skill id="reaction" category={data[0].category} icon={data[0].icon} score={data[0].score} />
+    <Skill id="memory" category={data[1].category} icon={data[1].icon} score={data[1].score} />
+    <Skill id="verbal" category={data[2].category} icon={data[2].icon} score={data[2].score} />
+    <Skill id="visual" category={data[3].category} icon={data[3].icon} score={data[3].score} />
     <Button/>
     </div>
 }
